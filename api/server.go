fff3ab5a105b628b77f7fef47e67e68ba3db7b8e
langchou <375783470@qq.com>
2024-09-04 13:00:51 +0800

    add:transfer api

--- api/server.go
+++ api/server.go
@@ -16,6 +16,7 @@ func NewServer(store db.Store) *Server {
 	router.POST("/accounts", server.createAccount)
 	router.GET("/accounts/:id", server.getAccount)
 	router.GET("/accounts", server.listAccounts)
+	router.POST("/transfer", server.createTransfer)
 	server.router = router
 	return server
 }
